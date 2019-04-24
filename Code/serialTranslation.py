2d0fd471ee8e858058a49c1a5986c2b3374b29e9
Armandt <fouriearmandt@gmail.com>
2019-04-24 13:30:48 +0200

    python file is finished
    Hy dump nou ook die data na 'n csv toe, met sensor values in hulle eie kolomme

--- Code/serialTranslation.py
+++ Code/serialTranslation.py
@@ -3,8 +3,21 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 import numpy as np
+import csv
 
 ser = serial.Serial()
+data = [[], [], [], [], []]
+pos = 0
+
+def dumpData(d, fname):
+    global data
+
+    d = np.array(data, dtype = float)
+    d = d.transpose()
+    dataFile = open(fname, 'wb')
+    with dataFile:
+        writer = csv.writer(dataFile)
+        writer.writerows(d)
 
 def setupSerial(baud):
     ser.baudrate = baud
@@ -15,15 +28,10 @@ def setupSerial(baud):
     ser.timeout = None
     ser.open()
 
-data = [[],[],[],[],[]]
+
 def animate(i):
     global data
-    if len(data[0]) > 2000:
-        data[0] = data[0][1000:]
-        data[1] = data[1][1000:]
-        data[2] = data[2][1000:]
-        data[3] = data[3][1000:]
-        data[4] = data[4][1000:]
+    global pos
     points = 0
     while ser.is_open and points < 50:
         line = ser.readline()
@@ -35,12 +43,20 @@ def animate(i):
                 data[a].append(data[a][len(data[a])-2]) #append prev voltage if too few bits
         points += 1
 
+    if len(data[0]) % 2000 == 0:
+        #data[0] = data[0][1000:]
+        #data[1] = data[1][1000:]
+        #data[2] = data[2][1000:]
+        #data[3] = data[3][1000:]
+        #data[4] = data[4][1000:]
+        pos += 1
+
     ax1.clear()
-    ax1.plot(data[0])
-    ax1.plot(data[1])
-    ax1.plot(data[2])
-    ax1.plot(data[3])
-    ax1.plot(data[4])
+    ax1.plot(data[0][2000 * pos:])
+    ax1.plot(data[1][2000 * pos:])
+    ax1.plot(data[2][2000 * pos:])
+    ax1.plot(data[3][2000 * pos:])
+    ax1.plot(data[4][2000 * pos:])
 
 
     return 
@@ -52,5 +68,6 @@ setupSerial(19600)
 plt.ylabel("Voltage")
 ani = animation.FuncAnimation(fig, animate, interval = 510)
 plt.show()
+dumpData(data, 'dataDump.csv')
 ser.close()
 
