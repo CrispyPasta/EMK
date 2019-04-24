0c1ded25c24170c00aedf0c79194585e0b695522
Armandt <fouriearmandt@gmail.com>
2019-04-24 16:55:41 +0200

    Update serialTranslation.py

--- Code/serialTranslation.py
+++ Code/serialTranslation.py
@@ -38,7 +38,7 @@ def animate(i):
         for a in range(0, 5):
             try:
                 voltage = 5 * ord(line[a]) / 255.0
-                data[a].append(voltage + 0.1 * a)
+                data[a].append(voltage)
             except:
                 data[a].append(data[a][len(data[a])-2]) #append prev voltage if too few bits
         points += 1
