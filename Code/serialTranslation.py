ce4599e1e0bbfb0170946f3a5ac412a7e62ccc7f
Armandt <fouriearmandt@gmail.com>
2019-04-24 17:01:03 +0200

    Python will always start now
    Maar hy begin dalk 'n 0 heel an die begin van execution

--- Code/serialTranslation.py
+++ Code/serialTranslation.py
@@ -40,7 +40,7 @@ def animate(i):
                 voltage = 5 * ord(line[a]) / 255.0
                 data[a].append(voltage)
             except:
-                data[a].append(data[a][len(data[a])-2]) #append prev voltage if too few bits
+                data[a].append(0) #append prev voltage if too few bits
         points += 1
 
     if len(data[0]) % 2000 == 0:
