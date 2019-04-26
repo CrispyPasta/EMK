94e1962c82b25ae2349f4fef0ed74ea74c937324
Armandt <fouriearmandt@gmail.com>
2019-04-26 19:44:51 +0200

    Update serialTranslation.py

--- Code/serialTranslation.py
+++ Code/serialTranslation.py
@@ -22,7 +22,7 @@ def dumpData(d, fname):
 def setupSerial(baud):
     ser.baudrate = baud
     ser.bytesize = 8
-    ser.port = 'COM6' 
+    ser.port = 'COM10' 
     ser.parity = 'N'
     ser.stopbits = 1
     ser.timeout = None
